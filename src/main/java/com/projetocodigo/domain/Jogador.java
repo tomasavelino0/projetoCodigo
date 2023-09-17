5318aa0e793d323e759b4003111d3d57733d35b5
tomas <tomasdc2016@gmail.com>
2023-09-16 23:28:05 -0300

    feat: ambiente inicial com as dependencias do spring boot, banco H2 e jpa e criacao da classe jogador

---
+++ src/main/java/com/projetocodigo/domain/Jogador.java
@@ -0,0 +1,16 @@
+package com.projetocodigo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name="jogadores")
+@Table()
+public class Jogador {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  private String nome;
+}
---
+++ src/test/java/com/projetocodigo/projetoIbm/ProjetoIbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetocodigo.projetoIbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
