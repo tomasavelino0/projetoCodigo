b9f3cf781f3d6a2adf9860ff601640e260246308
tomas <tomasdc2016@gmail.com>
2023-09-17 01:29:41 -0300

    feat: Criado a classe Jogador

--- src/main/java/com/projetocodigo/domain/Jogador.java
+++ src/main/java/com/projetocodigo/domain/Jogador.java
@@ -5,10 +5,21 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity(name="jogadores")
-@Table()
+@Entity(name = "jogadores")
+@Table(name = "jogadores")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+@NoArgsConstructor
 public class Jogador {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
