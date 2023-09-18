efcf5de26df436c4b4d45fcb6412024364fe3392
tomas <tomasdc2016@gmail.com>
2023-09-17 21:55:58 -0300

    fix: tentando resolver problema de CORS

--- src/main/java/com/projetocodigo/ProjetoIbmApplication.java
+++ src/main/java/com/projetocodigo/ProjetoIbmApplication.java
@@ -2,10 +2,8 @@ package com.projetocodigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
 public class ProjetoIbmApplication {
 
   public static void main(String[] args) {
